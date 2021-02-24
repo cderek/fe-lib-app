5da4b4f9510fb32f2ed270747f6b6d98c2747dfe
cderek <pealstyle@gmail.com>
2021-02-24 14:43:07 +0800

    feat:udpate

--- src/App.tsx
+++ src/App.tsx
@@ -7,21 +7,20 @@ function App() {
 
   return (
     <main>
-      <nav className="menu" tabIndex="0">
+      <nav className="menu">
         <div className="smartphone-menu-trigger"></div>
         <header className="avatar">
           <img src="https://assets.codepen.io/441030/internal/avatars/users/default.png" />
           <h2>derek</h2>
         </header>
         <ul>
-          <li tabIndex="0" className="iconfont iconicon-doc"><span>JavaScript防抖</span></li>
-          <li tabIndex="1" className="iconfont iconicon-doc"><span>JavaScript节流</span></li>
-          <li tabIndex="2" className="iconfont iconicon-doc"><span>JavaScript深浅拷贝</span></li>
-          <li tabIndex="3" className="iconfont iconicon-doc"><span>JavaScript数组去重</span></li>
+          <li className="iconfont iconicon-doc"><span>JavaScript防抖</span></li>
+          <li className="iconfont iconicon-doc"><span>JavaScript节流</span></li>
+          <li className="iconfont iconicon-doc"><span>JavaScript深浅拷贝</span></li>
+          <li className="iconfont iconicon-doc"><span>JavaScript数组去重</span></li>
         </ul>
       </nav>
     </main>
   )
 }
-
 export default App
